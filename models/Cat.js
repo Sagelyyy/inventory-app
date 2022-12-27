e46b6c51e0bb244038687d667f483dbecd2ea3ef
Chris <orj.tsuk@gmail.com>
2022-12-27 08:16:51 -0500

    added mixin cards for cats to homogenize the look across the site.

--- models/Cat.js
+++ models/Cat.js
@@ -3,22 +3,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CatSchema = new Schema({
-    name: {type: String, required: true, maxLength: 100},
-    age: {type: String, maxLength: 2},
-    color: {type: String, required: true, maxLength: 30},
-    desc: {type: String, maxLength: 100},
-    breed: {type: Schema.Types.ObjectId, ref:"Breed"},
-    gender: {
-        type: String,
-        required: true,
-        enum: ["Male", "Female"],
-        default: "Male"
-    },
-    image: {type: String}
-})
+  name: { type: String, required: true, maxLength: 100 },
+  age: { type: String, maxLength: 2 },
+  color: { type: String, required: true, maxLength: 30 },
+  desc: { type: String, maxLength: 100 },
+  breed: { type: Schema.Types.ObjectId, ref: "Breed" },
+  gender: {
+    type: String,
+    required: true,
+    enum: ["Male", "Female"],
+    default: "Male",
+  },
+  image: { type: String },
+  shelter: { type: Schema.Types.ObjectId, ref: "Shelter" },
+});
 
-CatSchema.virtual("url").get(function(){
-    return `/cat/${this._id}`
-})
+CatSchema.virtual("url").get(function () {
+  return `/cat/${this._id}`;
+});
 
-module.exports = mongoose.model("Cat", CatSchema)
+module.exports = mongoose.model("Cat", CatSchema);
--- models/Shelter.js
+++ models/Shelter.js
@@ -3,15 +3,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ShelterSchema = new Schema({
-    name: {type: String, required: true, maxLength: 100},
-    desc: {type: String, required: true, maxLength: 300},
-    location: {type: String, required: true, maxLength: 20},
-    current_cats: {type: [mongoose.Schema.ObjectId], ref: 'Cat'}
-
+  name: { type: String, required: true, maxLength: 100 },
+  desc: { type: String, required: true, maxLength: 300 },
+  location: { type: String, required: true, maxLength: 50 },
+  current_cats: { type: [mongoose.Schema.ObjectId], ref: "Cat" },
 });
 
 ShelterSchema.virtual("url").get(function () {
-    return `/shelter/${this._id}`
-})
+  return `/shelter/${this._id}`;
+});
 
-module.exports = mongoose.model("Shelter", ShelterSchema)
+module.exports = mongoose.model("Shelter", ShelterSchema);
